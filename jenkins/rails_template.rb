a1975e141bac7d2f0d7221fdaa01b61a4fe4fe93
Jeremy Duncan <jpd294@cornell.edu>
2025-04-14 14:58:29 -0400

    28: updated script

--- jenkins/rails_template.rb
+++ jenkins/rails_template.rb
@@ -1,4 +1,5 @@
 # Force local platform gem installation for Nokogiri
 run "bundle config set force_ruby_platform true"
 run "gem uninstall nokogiri -aIx || true"
+run "gem install nokogiri --platform=ruby"
 run "bundle install"
