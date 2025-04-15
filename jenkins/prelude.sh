4334bdd2a7227273cab71999f454fd4d3632a7a8
Jeremy Duncan <jpd294@cornell.edu>
2025-04-15 18:25:36 -0400

    28: updated script

--- jenkins/prelude.sh
+++ jenkins/prelude.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# bundler for automated tests
 source "scripts/log_utils.sh"
 set -e
-
 source jenkins/environment.sh
 
 print_header "💻 Executing prelude.sh"
@@ -10,24 +8,19 @@ print_header "💻 Executing prelude.sh"
 # Ensure the PATH includes the directory where Bundler is installed
 export PATH=$GEM_HOME/bin:$PATH
 
-# Force use sqlite version in jenkins
-#bundle config build.sqlite3 \
-#  --with-sqlite3-include=/usr/include \
-#  --with-sqlite3-lib=/usr/lib64
-
 # Verify Bundler installation
 print_msg "💠 which bundle: $(which bundle)"
 print_msg "💠 bundle --version: $(bundle --version)"
 
 # Avoid native extension issues like with Nokogiri + GLIBC mismatch
-rm -f Gemfile.
-export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
-
-# Ensure Nokogiri gets compiled locally (not precompiled)
-print_msg "💠 nokogiri info (before): $(gem list | grep nokogiri)"
-gem uninstall nokogiri -aIx || true
-gem install nokogiri --platform=ruby
-print_msg "💠 nokogiri info (after): $(bundle info nokogiri || echo 'nokogiri not found')"
+#rm -f Gemfile.
+#export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
+#
+## Ensure Nokogiri gets compiled locally (not precompiled)
+#print_msg "💠 nokogiri info (before): $(gem list | grep nokogiri)"
+#gem uninstall nokogiri -aIx || true
+#gem install nokogiri --platform=ruby
+#print_msg "💠 nokogiri info (after): $(bundle info nokogiri || echo 'nokogiri not found')"
 
 # Install base dependencies
 bundle install
