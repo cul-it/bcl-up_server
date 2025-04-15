c25a127352b37224b5ac42b98d23e59e5a85d84b
Jeremy Duncan <jpd294@cornell.edu>
2025-04-15 09:36:53 -0400

    28: updated script

--- jenkins/rails_template.rb
+++ jenkins/rails_template.rb
@@ -3,7 +3,14 @@ say_status :info, "##################################################", :blue
 say_status :info, "rails template executed", :green
 say_status :info, "##################################################", :blue
 
+say_status :info, "Uninstalling older nokogiri...", :blue
 run "gem uninstall nokogiri -aIx || true"
+
+say_status :info, "force_ruby_platform true...", :blue
 run "bundle config set force_ruby_platform true"
+say_status :info, "Install correct nokogiri...", :blue
+say_status :info, "##################################################", :blue
 run "gem install nokogiri --platform=ruby"
+say_status :info, "##################################################", :blue
+say_status :info, "bundle install...", :blue
 run "bundle _2.4.19_ install"
